362580167861822ac461a79374f6a2df5a122cc5
George Toothman <george.toothman@flatironschool.com>
2015-08-16 14:32:07 -0400

    all done son

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_accessor :isosceles, :equilateral, :scalene
+
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+    @unordered_array = [@side_one, @side_two, @side_three]
+  end
+
+  def kind
+    if @side_one <= 0 && @side_two <= 0 && @side_three <= 0
+      raise TriangleError
+    elsif @side_one + @side_two <= @side_three || @side_one + @side_three <= @side_two || @side_two + @side_three <= @side_one
+      raise TriangleError  
+    elsif (@side_one == @side_two && @side_two != @side_three) || (@side_one == @side_three && @side_two != @side_one) || (@side_two == @side_three && @side_three != @side_one)
+      :isosceles
+    elsif @side_one == @side_two && @side_two == @side_three
+      :equilateral
+    elsif @unordered_array.sort { |a, b| a <=> b } && @side_one != @side_two || @side_two != @side_three || @side_one != @side_three
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
 end
